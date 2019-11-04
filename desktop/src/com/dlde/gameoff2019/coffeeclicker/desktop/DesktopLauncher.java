c5b6c8ee65b7d3552518b22cadd854500ce54e39
Jesus Guereca Berumen <jesusguerecab@gmail.com>
2019-11-04 12:23:33 -0700

    libgdx project files

---
+++ desktop/src/com/dlde/gameoff2019/coffeeclicker/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dlde.gameoff2019.coffeeclicker.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dlde.gameoff2019.coffeeclicker.CoffeeClickerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CoffeeClickerGame(), config);
+	}
+}
